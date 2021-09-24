2fe8637cd5ca2fe65946ea3aea4dd2744c73459e
Nikita Bozhenkov <nikbozhenkov@gmail.com>
2021-09-25 00:54:54 +0300

    Added forward movement to player and camera

--- source/Assets/Scripts/Player/Movement.cs
+++ source/Assets/Scripts/Player/Movement.cs
@@ -6,26 +6,31 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     private Touch _touch;
+    [SerializeField] private float _speed;
     [SerializeField] private float _speedModifier;
 
     private void FixedUpdate()
     {
-        ProcessTouches();
+        if (!ProcessTouches())
+        {
+            transform.Translate(0, 0, _speed * Time.deltaTime);
+        }
     }
 
-    private void ProcessTouches()
+    private bool ProcessTouches()
     {
         List<Touch> touches = InputHelper.GetTouches();
-        if (touches.Count == 0) return;
+        if (touches.Count == 0) return false;
 
         _touch = touches[0];
-        if (_touch.phase != TouchPhase.Moved) return;
+        if (_touch.phase != TouchPhase.Moved) return false;
 
         Vector3 newPosition = transform.position;
         Vector2 additionalPosition = _touch.deltaPosition * _speedModifier;
         newPosition.x += additionalPosition.x;
         newPosition.z += additionalPosition.y;
         transform.position = newPosition;
+        return true;
     }
 
 }
